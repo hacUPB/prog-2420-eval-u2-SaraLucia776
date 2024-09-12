95c6132f5871d3f11c44a5acdfc9c040b39272a5
Sara Lucia Jara <santiago.londonos@upb.edu.com>
2024-09-12 09:48:29 -0500

    Modificaciones código reservas

--- src/reservas.py
+++ src/reservas.py
@@ -1,23 +1,38 @@
 import random 
+from datetime import datetime
 
 from random import randint
 def main():
    distancia = 0 
    costo = 0
-   título = input("Ingrese su título (Sr o Sra) ")
+   ciudades = ['Medellín', 'Bogotá', 'Cartagena']
+   dias = ['Lunes','Martes','Miércoles', 'Jueves','Viernes','Sábado','Domingo']
+   
+   título = input("Ingrese su título (Sr o Sra) ").capitalize()
    nombre_completo = input("Ingrese su nombre completo: ")
+   
    if título == ("Sr"):
      print(f"{título} {nombre_completo} Bienvenido a Sky Airlines")
    elif título == ("Sra"):
      print(f"{título}{nombre_completo} Bienvenido a Sky Airlines")
-   ciudades = ['Medellín', 'Bogotá', 'Cartagena']
+   
    origen = (input("Ingrese la ciudad de origen (Medellín, Bogotá, Cartagena)"))
-   if origen not in ciudades: 
-      print("Ciudad de origen incorrecta, seleccione nuevamente")
-   destino = (input("Ingrese la ciudad de destino (Medellín, Bogotá, Cartagena)"))
-   if destino not in ciudades: 
-      print("Ciudad de destino incorrecta, seleccione nuevamente")
-   elif origen == ciudades[0] and destino == ciudades[1]:
+   while True:
+      if origen not in ciudades: 
+         print("Ciudad de origen incorrecta, seleccione nuevamente")
+         origen = (input("Ingrese la ciudad de origen (Medellín, Bogotá, Cartagena)"))
+      else:
+         break
+   
+   destino = (input("Ingrese la ciudad de destino (Medellín, Bogotá, Cartagena ) "))
+   while True:
+      if destino not in ciudades: 
+         print("Ciudad de destino incorrecta, seleccione nuevamente")
+         destino = (input("Ingrese la ciudad de destino (Medellín, Bogotá, Cartagena ) "))
+      else:
+         break 
+   
+   if  origen == ciudades[0] and destino == ciudades[1]:
       distancia == 240
    elif origen == ciudades[1] and destino == ciudades[0]:
       distancia == 240 
@@ -32,7 +47,7 @@ def main():
    else: 
       print("Viaje inválido")
    
-   num_sem = int(input("Ingrese el día de la semana en el que viajará: \nLunes(1) \nMartes(2) \nMiércoles(3) \nJueves(4) \nViernes(5) \nSábado(6) \nDomingo(7): "))
+   ***num_sem = int(input("Ingrese el día de la semana en el que viajará: \nLunes(1) \nMartes(2) \nMiércoles(3) \nJueves(4) \nViernes(5) \nSábado(6) \nDomingo(7): "))
    num_dia = int(input("Ingrese el día del mes en el que viajará "))
    num_mes = int(input("Ingrese el número del mes en el que viajará "))
    if num_sem <= 4 and distancia < 400: 
@@ -44,12 +59,23 @@ def main():
    elif num_sem > 4 and distancia > 400:
       precio = 213000
    
-   Asiento = input("Ingrese el lugar de su preferencia: \nA: Ventana \nB: Sin preferencia \nC: Pasillo  ")
+   asientos = ['A', 'B', 'C']
+   asiento = (input("Ingrese el asiento de su preferencia (Ventana, Sin preferencia, Pasillo)"))
+   if asiento == "Ventana":
+      asiento = 'A'
+   elif asiento == "Sin preferencia":
+      asiento = 'B' 
+   elif asiento == "Pasillo":
+      asiento = 'C'
+   else: 
+      print("Seleccione una opción correcta")
+   
    Fila = randint(1,29)
-   print(f"{título} {nombre_completo} el vuelo será el {num_dia}/{num_mes} con la silla {Asiento} {Fila}, con un valor de {costo}. Su vuelo quedó reservado. Gracias por volar con nosotros")
+   print(f"{título} {nombre_completo} su vuelo será el {num_dia}/{num_mes} desde {origen} hacia {destino} con la silla {asiento} {Fila}, por un valor de {costo}. Su vuelo quedó reservado. Gracias por volar con nosotros")
 
    pass 
 
 
 if __name__ == "__main__":
     main()
+    
